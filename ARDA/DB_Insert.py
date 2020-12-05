ee6d2f4ca2c4fd9a57d1ad5fd40dc023e9f50d30
Merciful423 <ardagore423@gmail.com>
2020-12-05 15:21:47 +0300

    commit

---
+++ ARDA/DB_Insert.py
@@ -0,0 +1,20 @@
+import sqlite3 as sql
+try:
+    db = sql.connect(r"chinook\chinook.db") 
+    cur = db.cursor() 
+    # insert sorgusunu yazarak işlemi gerçekleştireceğiz. 
+    isim = input("İsmi giriniz:")
+    soyisim = input("Soyismi giriniz:")
+    email = input("Eposta adresini giriniz:")
+    sorgu = f"""INSERT INTO customers (FirstName,LastName,Email) VALUES ('{isim}','{soyisim}','{email}')"""
+    cur.execute(sorgu)
+    sorgu = """SELECT CustomerId FROM customers order by CustomerId DESC LIMIT 1"""
+    db.commit()
+    print(f"Kayıt Id {cur.execute(sorgu).fetchone()[0]}")
+except Exception as hata:
+    db.rollback()
+    print("Hata Mesajı :",hata)
+finally:
+    cur.close()
+    db.close()
+
